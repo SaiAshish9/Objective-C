558c923e7d91ef2eee2dc6451fa732149eabcff0
saiashish9 <saiashish3760@gmail.com>
2022-02-06 16:12:28 +0530

    updated Animal Object

---
+++ objC/objC/Koala.h
@@ -0,0 +1,16 @@
+//
+//  Koala.h
+//  objC
+//
+//  Created by Sai Ashish Darapureddy on 06/02/22.
+//
+
+#import "Animal.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Koala : Animal
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ objC/objC/Koala.m
@@ -0,0 +1,12 @@
+//
+//  Koala.m
+//  objC
+//
+//  Created by Sai Ashish Darapureddy on 06/02/22.
+//
+
+#import "Koala.h"
+
+@implementation Koala
+
+@end
