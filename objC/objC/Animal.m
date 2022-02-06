47310b5347e54bea8d43a4d387550372af0380b9
saiashish9 <saiashish3760@gmail.com>
2022-02-06 15:24:50 +0530

    k

--- objC/objC/Animal.m
+++ objC/objC/Animal.m
@@ -1,12 +1,45 @@
 //
 //  Animal.m
 //  objC
-//
 //  Created by Sai Ashish Darapureddy on 06/02/22.
 //
-
 #import "Animal.h"
 
 @implementation Animal
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.name = @"Sai";
+    }
+    return self;
+}
+
+- (instancetype)initWithName:(NSString *)defaultName
+{
+    self = [super init];
+    if (self) {
+        self.name = defaultName;
+    }
+    return self;
+}
+
+-(void) getInfo{
+NSLog(@"Random");
+}
+
+-(float) weightInKg:(float)weightInLbs {
+    return weightInLbs * 0.4535;
+}
+
+-(int) getSum:(int)num1 nextNumber:(int)num2 {
+    return num1 + num2;
+}
+
+-(NSString *)talkToMe:(NSString *)myName{
+    NSString *response = [NSString stringWithFormat:@"Hello %@", myName];
+    return response;
+}
 
 @end
