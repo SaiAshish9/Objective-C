a6d530eeae490184cc6373cf48560e5e90a940cc
saiashish9 <saiashish3760@gmail.com>
2022-02-09 19:52:53 +0530

    updated storyboard

--- OBJ_C_TableViews/OBJ_C_TableViews/Controller/ViewController.m
+++ OBJ_C_TableViews/OBJ_C_TableViews/Controller/ViewController.m
@@ -15,8 +15,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.myTableView.dataSource = self;
+    self.myTableView.delegate = self;
     // Do any additional setup after loading the view.
 }
 
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    cell.textLabel.text = @"Sai";
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+}
 
 @end
